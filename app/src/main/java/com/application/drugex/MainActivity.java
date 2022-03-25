7cc44c770211cc4d9d277b480c0235ff2a4f1a4c
Mohamed Inamul Hassan <mohamedinamulhassan@Mohameds-MacBook-Air.local>
2022-03-25 07:11:18 +0530

    first commit

---
+++ app/src/main/java/com/application/drugex/MainActivity.java
@@ -0,0 +1,22 @@
+package com.application.drugex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            startActivity(new Intent(MainActivity.this, ReportActivity.class));
+            finish();
+        }, 3000);
+    }
+}
