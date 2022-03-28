810b5c45211dea32c2304029a1b0a5ee0a168016
Inamul07 <mohammedinamul87@gmail.com>
2022-03-28 19:08:24 +0530

    updated TermsActivity

--- app/src/main/java/com/application/drugex/TermsActivity.java
+++ app/src/main/java/com/application/drugex/TermsActivity.java
@@ -37,6 +37,15 @@ public class TermsActivity extends AppCompatActivity {
             }
         });
 
+        btnDecline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveTaskToBack(true);
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);
+            }
+        });
+
     }
 
     private void start() {
